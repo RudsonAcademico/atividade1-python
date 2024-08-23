676b8a46b92d39a0014e6ede8d2aa7680bf97dba
RudsonAcademico <silva.rudson@academico.ifpb.edu.br>
2024-08-23 18:08:42 +0000

    arrumadinha

--- q4.py
+++ q4.py
@@ -5,11 +5,11 @@ obter os valores de a,b e c do usuário.
 Consideremos somente a obtenção de raízes reais.
 """
 
-a = float(input("Digite o valor de a: "))
-b = float(input("Digite o valor de b: "))
-c = float(input("Digite o valor de c: "))
-
-x1 = (-b + ((b**2 - 4*a*b)**1/2))/a*2
-x2 = (-b - ((b**2 - 4*a*b)**1/2))/a*2
+a = int(input("Digite o valor de a: "))
+b = int(input("Digite o valor de b: "))
+c = int(input("Digite o valor de c: "))
+delta = (b**2)-(4*a*b)
+x1 = ((b* -1)+(delta**1/2))/(a*2)
+x2 = ((b* -1)-(delta**1/2))/(a*2)
 
 print("x1 = {} e x2 = {}".format(x1,x2))
